0a926072193e9d003f94488605105a79a5f7534e
Jean David Vasquez <116390051+JeanDavidVasquez@users.noreply.github.com>
2024-12-19 07:52:56 -0500

    cambios

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"serviciosenfermeria-df5d5","appId":"1:732919500763:web:175017e658d518d9a8e1fe","storageBucket":"serviciosenfermeria-df5d5.appspot.com","apiKey":"AIzaSyB3v4322JLrRqeOp90ej2bB0s1gtSzoKZk","authDomain":"serviciosenfermeria-df5d5.firebaseapp.com","messagingSenderId":"732919500763","measurementId":"G-5QCVZ00XCT"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
